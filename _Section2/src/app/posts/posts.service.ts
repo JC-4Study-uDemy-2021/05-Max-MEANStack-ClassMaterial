b6be2513bd0fbfe8dd906e917ee1ecb8f917c722
Justin Heath <just.coer@outlook.com>
2021-08-03 16:09:22 +0100

    section 4

---
+++ _Section2/src/app/posts/posts.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+import { Post } from './post.model';
+
+@Injectable({providedIn: 'root'})
+export class PostsService {
+  private posts: Post[] = [];
+  private postsUpdated = new Subject<Post[]>();
+
+  getPosts() {
+    return [...this.posts];
+  }
+
+  getPostUpdateListener() {
+    return this.postsUpdated.asObservable();
+  }
+
+  // JH - it would be better to use 'post: Post' as the attribute pasted in here but I am not sure how it works
+  addPost(title: string, content: string) {
+    const post: Post = {title: title, content: content};
+    this.posts.push(post);
+    this.postsUpdated.next([...this.posts]);
+  }
+}
